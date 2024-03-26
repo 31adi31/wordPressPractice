3313fac610157623d815abc55d6507495f024e79
Aditya <31adi31@gmail.com>
2024-03-26 15:44:42 -0500

    userSecurityTest

--- wp-content/plugins/copyright-date-block/src/render.php
+++ wp-content/plugins/copyright-date-block/src/render.php
@@ -13,25 +13,24 @@
 // Get the current year.
 
 //function for validating the user
-/*function validateUser() {
+static function validateUser() {
 	if(current_user_can('edit_post')) {
-		
+		$block_content = '<p ' . get_block_wrapper_attributes() . '>© ' . esc_html( $display_date ) . '</p>';
+	} else {
+		$block_content = "access denied";
 	}
 }
-function displayBlockContent() {
-	
-}
-add_action();
-do_action(); */
+
+
 $current_year = date( "Y" );
 
 // Determine which content to display.
 if ( isset( $attributes['fallbackCurrentYear'] ) && $attributes['fallbackCurrentYear'] === $current_year ) {
 
 	// The current year is the same as the fallback, so use the block content saved in the database (by the save.js function).
-	if(current_user_can('activate_plugins')) {
+	
 		$block_content = $content;
-	}
+	
 		
 } else {
 
@@ -41,9 +40,10 @@ if ( isset( $attributes['fallbackCurrentYear'] ) && $attributes['fallbackCurrent
 	} else {
 		$display_date = $current_year;
 	}
-	if(current_user_can('activate_plugins')) {
-		$block_content = '<p ' . get_block_wrapper_attributes() . '>© ' . esc_html( $display_date ) . '</p>';
-	}
+	
+	
+	
 }
+do_action(validateUser());
 
 echo wp_kses_post( $block_content );
